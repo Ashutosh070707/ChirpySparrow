3d14f4608178b76a5515ffe25ae10ee486078c48
Ashutosh_Kumar <kumarashutosh5689@gmail.com>
2025-01-25 09:32:56 +0530

    changes in sidebar

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -33,124 +33,113 @@ export const Sidebar = () => {
       direction={"column"}
       position="fixed"
       left="0"
-      height="100vh"
+      height={{ base: "auto", md: "100vh" }} // Adjust height for different screen sizes
+      maxH="100vh" // Prevent sidebar from growing beyond viewport height
+      overflowY="auto" // Add vertical scrollbar if content overflows
       bg={colorMode === "dark" ? "gray.800" : "white"}
       width="16%"
-      alignItems="flex-start"
-      justifyContent="flex-start"
     >
-      <Flex
-        justifyContent="center"
-        w="full"
-        h="800px"
-        maxH="800px"
-        bg={colorMode === "dark" ? "gray.800" : "white"}
-        overflowY="auto"
-        borderRadius={10}
-        mb={"3%"}
-      >
-        <Flex direction="column" alignItems={"center"} justifyContent="center">
-          <Box alignItems="center" mt={"40px"} mb={"50px"}>
-            <Link
-              as={RouterLink}
-              to="/"
-              textDecoration="none"
-              _hover={{ textDecoration: "none" }}
+      <Flex direction="column" alignItems={"center"} justifyContent="center">
+        <Box alignItems="center" mt={"40px"} mb={"50px"}>
+          <Link
+            as={RouterLink}
+            to="/"
+            textDecoration="none"
+            _hover={{ textDecoration: "none" }}
+          >
+            <Text
+              fontSize="2xl"
+              fontWeight="bold"
+              fontFamily={"Comic Sans MS, Comic Sans, cursive"}
             >
-              <Text
-                fontSize="2xl"
-                fontWeight="bold"
-                fontFamily={"Comic Sans MS, Comic Sans, cursive"}
-              >
-                ChirpySparrow
-              </Text>
-            </Link>
-          </Box>
-          <Flex direction={"column"} alignItems={"flex-start"} gap={10}>
-            <Link
-              as={RouterLink}
-              to="/"
-              textDecoration="none"
-              _hover={{ textDecoration: "none" }}
-            >
-              <Flex gap={4}>
-                <AiFillHome size={26}></AiFillHome>
-                <Text fontSize={"lg"}>Home</Text>
-              </Flex>
-            </Link>
-            <Link
-              as={RouterLink}
-              to={"/search"}
-              textDecoration="none"
-              _hover={{ textDecoration: "none" }}
-            >
-              <Flex gap={4}>
-                <FaSearch size={26} />
-                <Text fontSize={"lg"}>Search</Text>
-              </Flex>
-            </Link>
-            <Link
-              as={RouterLink}
-              to={"/create"}
-              textDecoration="none"
-              _hover={{ textDecoration: "none" }}
-            >
-              <Flex gap={4}>
-                <Box border="3px solid white" borderRadius={10}>
-                  <HiPlus size={23} />
-                </Box>
-                <Text fontSize={"lg"}>Create</Text>
-              </Flex>
-            </Link>
+              ChirpySparrow
+            </Text>
+          </Link>
+        </Box>
+        <Flex direction={"column"} alignItems={"flex-start"} gap={10}>
+          <Link
+            as={RouterLink}
+            to="/"
+            textDecoration="none"
+            _hover={{ textDecoration: "none" }}
+          >
+            <Flex gap={4}>
+              <AiFillHome size={26}></AiFillHome>
+              <Text fontSize={"lg"}>Home</Text>
+            </Flex>
+          </Link>
+          <Link
+            as={RouterLink}
+            to={"/search"}
+            textDecoration="none"
+            _hover={{ textDecoration: "none" }}
+          >
+            <Flex gap={4}>
+              <FaSearch size={26} />
+              <Text fontSize={"lg"}>Search</Text>
+            </Flex>
+          </Link>
+          <Link
+            as={RouterLink}
+            to={"/create"}
+            textDecoration="none"
+            _hover={{ textDecoration: "none" }}
+          >
+            <Flex gap={4}>
+              <Box border="3px solid white" borderRadius={10}>
+                <HiPlus size={23} />
+              </Box>
+              <Text fontSize={"lg"}>Create</Text>
+            </Flex>
+          </Link>
 
-            <Link
-              as={RouterLink}
-              to={"/chat"}
-              textDecoration="none"
-              _hover={{ textDecoration: "none" }}
-            >
-              <Flex gap={4}>
-                <BsFillChatQuoteFill size={26} />
-                <Text fontSize={"lg"}>Message</Text>
-              </Flex>
-            </Link>
-            <Link
-              as={RouterLink}
-              to={`/${loggedInUser.username}`}
-              textDecoration="none"
-              _hover={{ textDecoration: "none" }}
-            >
-              <Flex gap={4}>
-                <RxAvatar size={26}></RxAvatar>
-                <Text fontSize={"lg"}>Profile</Text>
-              </Flex>
-            </Link>
+          <Link
+            as={RouterLink}
+            to={"/chat"}
+            textDecoration="none"
+            _hover={{ textDecoration: "none" }}
+          >
+            <Flex gap={4}>
+              <BsFillChatQuoteFill size={26} />
+              <Text fontSize={"lg"}>Message</Text>
+            </Flex>
+          </Link>
+          <Link
+            as={RouterLink}
+            to={`/${loggedInUser.username}`}
+            textDecoration="none"
+            _hover={{ textDecoration: "none" }}
+          >
+            <Flex gap={4}>
+              <RxAvatar size={26}></RxAvatar>
+              <Text fontSize={"lg"}>Profile</Text>
+            </Flex>
+          </Link>
 
-            <Link
-              as={RouterLink}
-              to={"/settings"}
-              textDecoration="none"
-              _hover={{ textDecoration: "none" }}
-            >
-              <Flex gap={4}>
-                <MdOutlineSettings size={26} />
-                <Text fontSize={"lg"}>Settings</Text>
-              </Flex>
-            </Link>
+          <Link
+            as={RouterLink}
+            to={"/settings"}
+            textDecoration="none"
+            _hover={{ textDecoration: "none" }}
+          >
+            <Flex gap={4}>
+              <MdOutlineSettings size={26} />
+              <Text fontSize={"lg"}>Settings</Text>
+            </Flex>
+          </Link>
 
-            <Link
-              as={RouterLink}
-              to={"/auth"}
-              onClick={() => logout()}
-              textDecoration="none"
-              _hover={{ textDecoration: "none" }}
-            >
-              <Flex gap={4}>
-                <FiLogOut size={26} />
-                <Text fontSize={"lg"}>Logout</Text>
-              </Flex>
-            </Link>
-          </Flex>
+          <Link
+            as={RouterLink}
+            to={"/auth"}
+            onClick={() => logout()}
+            textDecoration="none"
+            _hover={{ textDecoration: "none" }}
+          >
+            <Flex gap={4}>
+              <FiLogOut size={26} />
+              <Text fontSize={"lg"}>Logout</Text>
+            </Flex>
+          </Link>
         </Flex>
       </Flex>
     </Flex>
